c65ceebba10e6f334007593ddf56c0318b8ac8b7
namnguyen315 <namnguyen3152001xtnd@gmail.com>
2024-09-09 16:26:47 +0700

    add lib richmarkdown

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}, ./node_modules/submarkdownntn/dist/cjs/src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
